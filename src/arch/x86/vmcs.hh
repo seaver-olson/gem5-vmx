d88a418ea137721e702c3f5b454bb911a057eff2
Seaver Olson <seaver@Seavers-MacBook-Air.local>
2026-03-31 19:53:22 -0500

    added static_asserts() to prevent malformed VMCS header on different systems

--- src/arch/x86/vmcs.hh
+++ src/arch/x86/vmcs.hh
@@ -7,12 +7,13 @@ namespace gem5
 {
 namespace X86ISA
 {
-class Vmcs
+// Intel VMX docs specify Vmcs section can contain at most 4KB
+class alignas(4096) Vmcs
 {
   public: 
     static constexpr size_t VmcsRegionSize = 4096; // 4K-Byte aligned memory
-
-    enum class LaunchState {
+                                                   
+    enum class LaunchState : uint8_t {
       Clear,
       Launched
     };
@@ -20,10 +21,18 @@ class Vmcs
     class VmcsHeader
     {
       public:
-        uint32_t revisionId;
-        uint32_t abortIndicator;
-    }
+        uint32_t revisionId; // Bit 30:0 Revision Identifier - Processors that maintain VMCS data in different formats use different revision identifiers
+                             // Bit 31 indicates whether the VMCS is a shadow VMCS (Section 27.10)
+                             // revisionId is NEVER written by the processor
+                             // software can discover the revisionId that a processor uses by reading the VMX capability MSR IA32_VMX_BASIC (Appendix A.1)
+        uint32_t abortIndicator; // Any non-zero number means proccessor threw abort signal
+    };
     
+    // Compile-Time Error handling to ensure perfect header on all systems
+    static_assert(sizeof(VmcsHeader) == 8, "VMCS header must be exactly 8 bytes");
+    static_assert(alignof(VmcsHeader) <= 4, "Unexpected alignment");
+    static_assert(offsetof(VmcsHeader, abortIndicator) == 4, "abortIndicator must be at byte 4");
+
 };
 }
 }
